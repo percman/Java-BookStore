02873551d45271802636bc76be5b2fcc7463b4af
Daniel Abramson <daniel.abramson@ruralsource.com>
2019-11-05 16:42:20 -0700

    Completed module 1

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -59,6 +59,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/insert":
 					insertBook(request, response);
           break;
+				case "/delete":
+					deleteBook(request, response);
+					break;
         default:
 				   listBooks(request, response);
            break;
@@ -116,4 +119,9 @@ public class ControllerServlet extends HttpServlet {
 
 	}
 
+	private void deleteBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+		int id = Integer.parseInt(request.getParameter("id"));
+		bookDAO.deleteBook(id);
+		response.sendRedirect("list");
+	}
 }
